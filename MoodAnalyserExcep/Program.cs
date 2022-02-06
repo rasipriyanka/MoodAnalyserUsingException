0964b56a9fd3df2d6f426e2cd2d1a2781f203661
rasipriyanka <priyankapendela111@gmail.com>
2022-02-06 17:10:34 +0530

    [Priyanka]Added the implementation

---
+++ MoodAnalyserExcep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserExcep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exceptions");
+        }
+    }
+}
